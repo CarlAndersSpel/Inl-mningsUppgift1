d791a65ba00bd96c41ab236908229ba480e814fe
CarlAndersSpel <caar5853@student.su.se>
2026-04-25 18:27:55 +0200

    Update MyEdge.java

--- Inl-mningsUppgift1/prog2-inlupp-template/backend/src/main/java/se/su/inlupp/MyEdge.java
+++ Inl-mningsUppgift1/prog2-inlupp-template/backend/src/main/java/se/su/inlupp/MyEdge.java
@@ -25,10 +25,22 @@ public class MyEdge<T> implements Edge<T>{
   }
 
   public void setWeight(int weight) {
+  if (weight < 0) {
+  throw new IllegalArgumentException();
+  }
+    this.weight = weight;
+
   }
 
   @Override
   public String toString() {
-    return String.format("%s (%s: %f)", this.destination, this.name, this.weight);
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("till " + this.destination + " ");
+    sb.append("med " + this.name + " ");
+    sb.append("tar " + this.weight + " ");
+    sb.append("\n");
+    //return String.format("%s (%s: %f)", this.destination, this.name, this.weight);
+    return sb.toString();
   }
 }  
