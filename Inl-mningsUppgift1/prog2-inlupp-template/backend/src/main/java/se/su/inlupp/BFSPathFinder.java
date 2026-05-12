5cf9fbcec60b2b7f8069f8fb36c9c82726f100ca
CarlAndersSpel <caar5853@student.su.se>
2026-05-12 11:54:39 +0200

    Update BFSPathFinder.java

--- Inl-mningsUppgift1/prog2-inlupp-template/backend/src/main/java/se/su/inlupp/BFSPathFinder.java
+++ Inl-mningsUppgift1/prog2-inlupp-template/backend/src/main/java/se/su/inlupp/BFSPathFinder.java
@@ -1,10 +1,54 @@
 package se.su.inlupp;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Map;
 
 public class BFSPathFinder<T> implements PathFinder<T> {
 
+    final Map<T, Boolean> isVisited = new HashMap<>();
+      MyPath<T> path;
+  boolean found = false;
+
   @Override
   public Path<T> findPath(Graph<T> graph, T from, T to) {
-    throw new UnsupportedOperationException("Unimplemented method 'findPath'");
+     Map<T, T> connections = new HashMap<>();
+    connections.put(from, null);
+    LinkedList<T> queue = new LinkedList<>();  //an arraylist istället kanske
+
+    path = new MyPath<>(from);
+    //bfs();
+    queue.add(from);
+    return null;
+
+    //throw new UnsupportedOperationException("Unimplemented method 'findPath'");
   }
+
+  private void bfs(ArrayList<ArrayList<Integer>> adj){
+  int V = adj.size();
+  boolean[] visited = new boolean[V];
+  ArrayList<Integer> res = new ArrayList<>();
+
+  int src = 0;
+  Queue<Integer> q = new LinkedList<>();
+  visited[src] = true;
+  q.add(src);
+
+  while (!q.isEmpty()) {
+    int curr = q.poll();
+    res.add(curr);
+
+     for ( int x : adj.get(curr)) {
+      if (!visited[x]) {
+        visited[x] = true;
+        q.add(x);
+      }
+     }
+
+
+  }
+   //return res;
+}
 }
 
