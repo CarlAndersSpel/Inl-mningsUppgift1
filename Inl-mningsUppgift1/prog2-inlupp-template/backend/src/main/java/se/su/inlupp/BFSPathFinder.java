9b147a3e80be07aae8cc5734fdfed3589eca7218
CarlAndersSpel <caar5853@student.su.se>
2026-05-12 17:24:40 +0200

    Update BFSPathFinder.java

--- Inl-mningsUppgift1/prog2-inlupp-template/backend/src/main/java/se/su/inlupp/BFSPathFinder.java
+++ Inl-mningsUppgift1/prog2-inlupp-template/backend/src/main/java/se/su/inlupp/BFSPathFinder.java
@@ -1,31 +1,63 @@
 package se.su.inlupp;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Queue;
+import java.lang.Iterable;
 import java.util.Map;
 
 public class BFSPathFinder<T> implements PathFinder<T> {
 
     final Map<T, Boolean> isVisited = new HashMap<>();
       MyPath<T> path;
-  boolean found = false;
+      boolean found = false;
 
   @Override
   public Path<T> findPath(Graph<T> graph, T from, T to) {
-     Map<T, T> connections = new HashMap<>();
+    //try {
+    Map<T, T> connections = new HashMap<>();
     connections.put(from, null);
     LinkedList<T> queue = new LinkedList<>();  //an arraylist istället kanske
 
-    path = new MyPath<>(from);
+   // path = new MyPath<>(from);
     //bfs();
     queue.add(from);
-    return null;
+    while (!queue.isEmpty() || !connections.containsKey(to)){
 
+      T current = queue.poll();//tar själva kön och lägger in och stryker bort.
+   for(Edge<T> edge : graph.getEdgesFrom(current)) {
+         T next = edge.getDestination();
+         if(!connections.containsKey(to) || !connections.containsKey(next)){
+          connections.put(next, current);
+          queue.add(next);
+         } 
+        }
+   }
+      MyPath<T> path = new MyPath<T>(from);
+
+      T current  = to;
+      while(current != null || !current.equals(from) ){
+        T next = connections.get(current);
+
+        Edge<T> edge = graph.getEdgeBetween(next, current);
+        path.addEdge(edge);
+        current = next;
+      } 
+      //T next = new getDestination<>(); 
+      // for ( Edge<T> edge : graph.getEdgesFrom(to))
+
+      return path;
+    }
+
+    //return null;
+
+      //LinkedList<Edge<T>> edges = new LinkedList<>();
+      //LinkedList<T> nodes = new LinkedList<>();
     //throw new UnsupportedOperationException("Unimplemented method 'findPath'");
   }
 
-  private void bfs(ArrayList<ArrayList<Integer>> adj){
+  /*private void bfs(ArrayList<ArrayList<Integer>> adj){
   int V = adj.size();
   boolean[] visited = new boolean[V];
   ArrayList<Integer> res = new ArrayList<>();
@@ -49,6 +81,10 @@ public class BFSPathFinder<T> implements PathFinder<T> {
 
   }
    //return res;
-}
-}
+
+
+   //return new PathClass<T>(from, to, edges, nodes);
+   //} catch (nosuchelementexception e ) { return null;}
+}*/
+
 
