6f5cf7dfcec7968eb0c929fdbbc59cf5b5a78b79
Erik Jakobsson <erik.l.jakobsson@gmail.com>
2026-05-18 16:30:57 +0200

    Revert "do not touch"
    This reverts commit 30785d3856d5da59ae0775e44b18a1fba8dc8bba.

--- Inl-mningsUppgift1/prog2-inlupp-template/backend/src/main/java/se/su/inlupp/BFSPathFinder.java
+++ Inl-mningsUppgift1/prog2-inlupp-template/backend/src/main/java/se/su/inlupp/BFSPathFinder.java
@@ -1,5 +1,9 @@
 package se.su.inlupp;
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Map;
 
 public class BFSPathFinder<T> implements PathFinder<T> {
 
@@ -9,148 +13,42 @@ public class BFSPathFinder<T> implements PathFinder<T> {
 
   @Override
   public Path<T> findPath(Graph<T> graph, T from, T to) {
+     Map<T, T> connections = new HashMap<>();
+    connections.put(from, null);
+    LinkedList<T> queue = new LinkedList<>();  //an arraylist istället kanske
 
-    if (!graph.hasNode(from) || !graph.hasNode(to)) {
-        return null;
-    }
-
-    Queue<T> queue = new LinkedList<>();
-
-    Map<T, T> connections = new HashMap<>();
-
-    Set<T> visited = new HashSet<>();
-
+    path = new MyPath<>(from);
+    //bfs();
     queue.add(from);
-    visited.add(from);
+    return null;
 
-    boolean found = false;
+    //throw new UnsupportedOperationException("Unimplemented method 'findPath'");
+  }
 
-    while (!queue.isEmpty() && !found) {
+  private void bfs(ArrayList<ArrayList<Integer>> adj){
+  int V = adj.size();
+  boolean[] visited = new boolean[V];
+  ArrayList<Integer> res = new ArrayList<>();
 
-        T current = queue.poll();
+  int src = 0;
+  Queue<Integer> q = new LinkedList<>();
+  visited[src] = true;
+  q.add(src);
 
-        for (Edge<T> edge : graph.getEdgesFrom(current)) {
+  while (!q.isEmpty()) {
+    int curr = q.poll();
+    res.add(curr);
 
-            T neighbor = edge.getDestination();
-
-            if (!visited.contains(neighbor)) {
-
-                visited.add(neighbor);
-
-                connections.put(neighbor, current);
-
-                queue.add(neighbor);
-
-                if (neighbor.equals(to)) {
-                    found = true;
-                    break;
-                }
-            }
-        }
-    }
-
-    if (!found && !from.equals(to)) {
-        return null;
-    }
-
-    List<T> nodes = new ArrayList<>();
-
-    T current = to;
-
-    nodes.add(current);
-
-    while (!current.equals(from)) {
-        current = connections.get(current);
-
-        if (current == null) {
-            return null;
-        }
-
-        nodes.add(current);
-    }
-
-    Collections.reverse(nodes);
-
-    MyPath<T> path = new MyPath<>(from);
-
-    for (int i = 0; i < nodes.size() - 1; i++) {
-
-        T a = nodes.get(i);
-        T b = nodes.get(i + 1);
-
-        Edge<T> edge = graph.getEdgeBetween(a, b);
-
-        path.addEdge(edge);
-    }
-
-    return path;
-}
-
-  static void bfs(List<List<Integer> > graph, int S,
-    List<Integer> par, List<Integer> dist)
-{
-
-Queue<Integer> q = new LinkedList<>();
-
-dist.set(S, 0);
-
-q.add(S);
+     for ( int x : adj.get(curr)) {
+      if (!visited[x]) {
+        visited[x] = true;
+        q.add(x);
+      }
+     }
 
 
-while (!q.isEmpty()) {
-
-int node = q.poll();
-
-
-for (int neighbor : graph.get(node)) {
-
-if (dist.get(neighbor)
-    == Integer.MAX_VALUE) {
-
-    par.set(neighbor, node);
-
-    dist.set(neighbor, dist.get(node) + 1);
-
-    q.add(neighbor);
-}
-}
+  }
+   //return res;
 }
 }
 
-static List<Integer> shortestPath(List<List<Integer>> graph,
-  int S, int D, int V)
-{
-
-List<Integer> par =
-new ArrayList<>(Collections.nCopies(V, -1));
-
-List<Integer> dist =
-new ArrayList<>(Collections.nCopies(V, Integer.MAX_VALUE));
-
-
-bfs(graph, S, par, dist);
-
-
-if (dist.get(D) == Integer.MAX_VALUE) {
-return new ArrayList<>(); 
-}
-
-
-List<Integer> path = new ArrayList<>();
-
-int currentNode = D;
-
-while (currentNode != -1) {
-path.add(currentNode);
-currentNode = par.get(currentNode);
-}
-
-
-Collections.reverse(path);
-
-return path;
-}
-
-
-
-}
